617235f22d700b61359f5b4a39980af0ae38926b
Muhammad Ardan Hilal <muhammadardanhilal@MacBook-Air-Muhammad-2.local>
2024-08-27 22:43:09 +0700

    initial commit zentask app

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+}
+
+// Top-level build.gradle.kts file
+buildscript {
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
+    }
+}
